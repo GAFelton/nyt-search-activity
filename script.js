3aabd0e3333ae8eb7bd2d20957cf4f306b02d986
ShepLT1 <lshepherd234@gmail.com>
2020-07-16 21:10:07 -0700

    form on click function with record limit loop. defined searchTerm variable

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
 var nytAPIKey = "ArZ61DBpDLUgKD0uZOIYcvLXHCmtc8qR";
 
+var searchTerm = $("#search-term-entry").val().trim();
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
   searchTerm + "&api-key=" + nytAPIKey;
@@ -11,6 +12,18 @@ $.ajax({
   method: "GET"
 })
   .then(function(response) {
+    
+    $("form").on("submit", function() {
+      
+      var recordNumber = $("record-number").val();
+    
+      for (i = 0; i < recordNumber; i++) {
+
+        $("top-articles").append(response.docs[i]);
+
+    }
+
+  })
 
   });
 });
